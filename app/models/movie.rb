8f48def12b932a64f98c89c46700185fa67e6fa1
filipe dias <filipediasit@gmail.com>
2022-07-09 12:18:16 +0100

    models and validations done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  validates :title, :overview, presence: true
+
+  # private
+
+  # def prevent_destroy
+  #   if
+  # end
 end
